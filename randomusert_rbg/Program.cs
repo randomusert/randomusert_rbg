f746680eb0e3183f82ef9caebaaa2c3057cd8bf2
randomusert <127417963+randomusert@users.noreply.github.com>
2023-05-18 13:13:05 +0300

    initial commit

---
+++ randomusert_rbg/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace randomusert_rbg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
