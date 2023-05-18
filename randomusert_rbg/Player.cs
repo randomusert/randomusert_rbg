4252496a70393cd8b293dbe3c5869c9082a8bd53
randomusert <127417963+randomusert@users.noreply.github.com>
2023-05-18 14:37:22 +0300

    player creation system added
    some improments also added

---
+++ randomusert_rbg/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace randomusert_rbg
+{
+    internal class Player
+    {
+        public string name;
+        public int coins = 0;
+        public int health = 100;
+        public int damage = 1;
+        public int armorValue = 0;
+        public int potion = 5;
+    }
+}
--- randomusert_rbg/Program.cs
+++ randomusert_rbg/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        public static Player currentPlayer = new Player();
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Start();
+        }
+
+        static void Start()
+        {
+            Console.WriteLine("randomusert's rbg based on console");
+            Console.WriteLine("What will be your player name?");
+            Console.WriteLine("Name:");
+            currentPlayer.name = Console.ReadLine();
+            Console.WriteLine("Welcome,"+ currentPlayer.name);
+            Console.WriteLine("You were outside when atack came and well atacked the village!");
+            Console.WriteLine("What thinked then?");
+
         }
     }
 }
